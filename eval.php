a7c498d6fc2e1456ddd6bf6f7fde6b1616302d14
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-09-26 17:04:41 +0530

    completed basic

--- eval.php
+++ eval.php
@@ -1,4 +1,5 @@
 <html>
+<body>
 <?php
 //sg
 if( !isset( $_SESSION ) ) {
@@ -7,19 +8,32 @@ if( !isset( $_SESSION ) ) {
 }
 if(isset($_SESSION['reloaded']) && isset($_SESSION['count'])) {
     $_SESSION['index'] = $_SESSION['index'] + 1;
-    if($_SESSION['index'] >= $_SESSION['count']) {
+    echo "Previous was " . $_POST["valid"]. "<br/>";
+    if($_SESSION['index'] >= $_SESSION['count']) { //done with the matches
         $_SESSION['index'] = 0;
+        session_destroy();
     }
-    echo "Index : " + $_SESSION['index'] + "<br/>";
+    //echo "Index : " + $_SESSION['index'] + "<br/>";
 } else {
     $_SESSION['reloaded'] = 1;
     $_SESSION['matches'] = file("sampledMatches.tsv");
-    $_SESSION['count'] = 10;//count($_SESSION['matches']);
-    echo $_SESSION['count'];
+    $_SESSION['count'] = count($_SESSION['matches']);
     $_SESSION['index'] = 1;
-    echo "xIndex : " + $_SESSION['index'] + "<br/>";
+    //echo "xIndex : " + $_SESSION['index'] + "<br/>";
 }
 
-print_r($_SESSION['matches'][$_SESSION['index']]);
+$matchSplit = explode("\t", $_SESSION['matches'][$_SESSION['index']]);
+$countryName = $matchSplit[4];
+$number = $matchSplit[5];
+$relation = $matchSplit[10];
+$sentence = $matchSplit[11];
+echo $sentence."<br/>".$countryName." ".$number." ".$relation."<br/>";
+echo "<form method = 'post' action = eval.php>";
+echo "Is Valid : Yes <input type = 'radio' name = 'valid' value = 'yes'>";
+echo " No <input type = 'radio' name = 'valid' value = 'no'>";
+echo "<input type='submit' name='submit' value='Next'>";
+echo "</form>";
+
 ?>
+</body>
 </html>
