070a93333ba5a89e85833f449ec44ae7825d2667
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-09-28 00:44:55 +0530

    separate db code from php

--- eval.php
+++ eval.php
@@ -1,38 +1,36 @@
 <?php
+include 'insert_db.php';
+if(!isset($_SESSION)) {
+    session_start();
+}
 if(isset($_SESSION['reloaded']) && isset($_SESSION['count'])) {
     //code to insert into database	
-	//insertToDb($_SESSION['dbhandle'], $_SESSION['matches'][$_SESSION['index']], $_POST['valid'], 1);
+	insertToDb($_SESSION['dbhandle'], $_SESSION['matches'][$_SESSION['index']], $_POST['valid'], 1);
 
     $_SESSION['index'] = $_SESSION['index'] + 1;
     if($_SESSION['index'] >= $_SESSION['count']) { //done with the matches
         $_SESSION['index'] = 0;
-	    //mysqli_close($_SESSION['dbhandle']);	//closing the connection.	
+	    mysqli_close($_SESSION['dbhandle']);	//closing the connection.	
         session_destroy();
         echo "Ok bye.";
         return;
     }
 } else {
-    echo "setting again<br/>";
-    session_start();
 	$username = "aman";
 	$password = "";
 	$hostname = "localhost";
 	//$db = "test";
 	//connection to the database
-	//$dbhandle = mysqli_connect($hostname, $username, $password, $db)
-  	//	or die("Unable to connect to MySQL");
-	//$_SESSION['dbhandle'] = $dbhandle;
+	$dbhandle = mysqli_connect($hostname, $username, $password, $db)
+  		or die("Unable to connect to MySQL");
+	$_SESSION['dbhandle'] = $dbhandle;
     $_SESSION['reloaded']=1;
     $file = "sampledMatches.tsv";
     $_SESSION['matches'] = file($file);
     $_SESSION['count'] = count($_SESSION['matches']);
     $_SESSION['index'] = 0;
 }
-if(isset($_SESSION['reloaded']) && isset($_SESSION['count'])) {
-    echo "Set now here<br/>";
-}
  
-echo "Index : ".$_SESSION['index'];
 $matchSplit = explode("\t", $_SESSION['matches'][$_SESSION['index']]);
 $countryName = $matchSplit[4];
 $number = $matchSplit[5];
@@ -42,6 +40,19 @@ $sentence = $matchSplit[11];
 <?php echo "<br/>".$sentence."<br/>".$countryName." ".$number." ".$relation."<br/>"; ?>
 <html>
 <body>
+<head>
+<!-- Latest compiled and minified CSS -->
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<div class = 'container'>
 <form role = 'form' method = 'post' action = 'eval.php'>
 <div class = 'form-group'>
 <div class = 'checkbox'>
